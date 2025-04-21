d878a3dc2aa73c85e9effe2845ddc9d9bf516ba7
VitorCardoso14 <vitorcardoso1401@gmail.com>
2025-04-21 19:22:06 +0000

    adicionei o if e else

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -88,11 +88,11 @@ int main() {
  printf("Código da Carta: %s\n", Codigo_da_Carta1);
  printf("Nome da Cidade: %s\n", Nome_da_Cidade1);
  printf("População: %lu\n", Populacao1);
- printf("Área(em km²): %.2f\n", Area1);
- printf("PIB: %.2f\n", PIB1);
+ printf("Área: %.2f km²\n", Area1);
+ printf("PIB: %.2f bilhões de reais\n", PIB1);
  printf("Números de Pontos Turísticos: %d\n", Numero_de_Pontos_Turisticos1);
  printf("Densidade Populacional: %.2f\n", Densidade_Populacional1);
- printf("PIB per Capita: %.2f\n", PIB_per_Capita1);
+ printf("PIB per Capita: %.2f reais\n", PIB_per_Capita1);
  printf("Super Poder: %.1f\n\n", Super_Poder1);
 
  printf("Carta 2\n");
@@ -100,17 +100,54 @@ int main() {
  printf("Código da Carta: %s\n", Codigo_da_Carta2);
  printf("Nome da Cidade: %s\n", Nome_da_Cidade2);
  printf("População: %lu\n", Populacao2);
- printf("Área(em km²): %.2f\n", Area2);
- printf("PIB: %.2f\n", PIB2);
+ printf("Área: %.2f km²\n", Area2);
+ printf("PIB: %.2f bilhões de reais\n", PIB2);
  printf("Números de Pontos Turísticos: %d\n", Numero_de_Pontos_Turisticos2);
  printf("Densidade Populacional: %.2f\n", Densidade_Populacional2);
- printf("PIB per Capita: %.2f\n", PIB_per_Capita2);
+ printf("PIB per Capita: %.2f reais\n", PIB_per_Capita2);
  printf("Super Poder: %.1f\n\n", Super_Poder2);
 
  if(Populacao1 > Populacao2){
-    printf("Carta 1 Ganhou!\n");
+    printf("Carta 1 (%s) Ganhou em População!\n", Nome_da_Cidade1);
  }else{
- printf("Carta 2 Ganhou!\n");
+ printf("Carta 2 (%s) Ganhou População!\n", Nome_da_Cidade2);
  }
+
+ if(Area1 > Area2){
+   printf("Carta 1 (%s) Ganhou em Área!\n", Nome_da_Cidade1);
+}else{
+printf("Carta 2 (%s) Ganhou em Área!\n", Nome_da_Cidade2);
+}
+
+if(PIB1 > PIB2){
+   printf("Carta 1 (%s) Ganhou em PIB!\n", Nome_da_Cidade1);
+}else{
+printf("Carta 2 (%s) Ganhou em PIB!\n", Nome_da_Cidade2);
+}
+
+if(Numero_de_Pontos_Turisticos1 > Numero_de_Pontos_Turisticos2){
+   printf("Carta 1 (%s) Ganhou em Pontos Turisticos!\n", Nome_da_Cidade1);
+}else{
+printf("Carta 2 (%s) Ganhou em Pontos Turisticos!\n", Nome_da_Cidade2);
+}
+
+if(Densidade_Populacional1 > Densidade_Populacional2){
+   printf("Carta 1 (%s) Ganhou em Densidade Populacional!\n", Nome_da_Cidade1);
+}else{
+printf("Carta 2 (%s) Ganhou em Densidade Populacional!\n", Nome_da_Cidade2);
+}
+
+if(PIB_per_Capita1 > PIB_per_Capita2){
+   printf("Carta 1 (%s) Ganhou em PIB per Capita!\n", Nome_da_Cidade1);
+}else{
+printf("Carta 2 (%s) Ganhou em PIB per Capita!\n", Nome_da_Cidade2);
+}
+
+if(Super_Poder1 > Super_Poder2){
+   printf("Carta 1 (%s) Ganhou no Super Poder!\n", Nome_da_Cidade1);
+}else{
+printf("Carta 2 (%s) Ganhou no Super Poder!\n", Nome_da_Cidade2);
+}
+
     return 0;
 }
